660d5565f024c0904fee342f053f4624269684f7
Elena Pushkar <elena@epushkar.com>
2021-06-18 10:59:34 -0500

    dispalaying orders for selected customers

--- src/Order.js
+++ src/Order.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ItemssList from './ItemsList';
 
 
 class Order extends React.Component{
@@ -11,50 +12,38 @@ class Order extends React.Component{
     }
   }
 
-
   async componentDidUpdate(prevProps){
     if(prevProps.selectedCustomerId !== this.props.selectedCustomerId) {
       const customer = (await axios.get(`/api/customers/${this.props.selectedCustomerId}`)).data;
-      this.setState({ customer })
-    }
-   
-    
+      const orders = (await axios.get(`/api/customers/${this.props.selectedCustomerId}/orders`)).data;
+      const ordersNameList = orders.map((a) => <li key={a.itemId}> {a.item.name} </li>)
+      const orderLength = orders.length;
+
+      this.setState({ customer, orderLength, ordersNameList })
+    } 
   }
+  
   async componentDidMount() {
     const customer = (await axios.get(`/api/customers/${this.props.selectedCustomerId}`)).data;
     const orders = (await axios.get(`/api/customers/${this.props.selectedCustomerId}/orders`)).data;
-    
-
-
-    this.setState({ customer, orders })
+    const ordersNameList = orders.map((a) => <li key={a.itemId}> {a.item.name} </li>)
+    const orderLength = orders.length;
+  
+    this.setState({ customer, orders, orderLength, ordersNameList })
   }
 
   render(){
-    const { customer, orders } = this.state
-    return (
-     
+    const { customer, orderLength, orders, ordersNameList } = this.state
+    return (   
     <div>
-       <h3> Orders ({ orders.length }) </h3>
+       <h3> { customer.name }'s Orders ({ orderLength }) </h3>
        <div className="orderDetails">
-         
-         <p> Order has been created: {} </p>
-         { customer.name }
-
+         { ordersNameList } 
        </div>
-    
     </div>
     )
-
   }
 }
 
-
-// const Order = () => {
-//   return (
-//     <div>
-//         <h3> Orders </h3>
-//     </div>
-//   )
-// };
 
 export default Order;
