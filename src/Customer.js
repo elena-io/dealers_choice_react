4f82f49e058ed51960ff4a1cbcfc8d4e079ca956
Elena Pushkar <elena@epushkar.com>
2021-06-18 15:56:43 -0500

    added styles

--- src/Customer.js
+++ src/Customer.js
@@ -2,12 +2,16 @@ import React from 'react';
 import App from './App';
 
 
+  const selectedCustomerId = window.location.hash.slice(1)
+
+
 const Customer = (props) => {
     const name = props.customer.name
     const id = props.customer.id
+    const selectedCustomerId = window.location.hash.slice(1)
 
     return (
-        <div>
+        <div className= { id === selectedCustomerId ? 'selected' : ''}>
             <a href={`#${id}`} >
                 { name }
             </a>
